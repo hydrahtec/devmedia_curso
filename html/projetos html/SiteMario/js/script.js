fcfd2b7088434e91d131282560b0ca4e385d292e
Eliel S <hydrahtec@gmail.com>
2023-11-12 21:12:59 -0300

    pagina mario
    html, css, js

---
+++ html/projetos html/SiteMario/js/script.js
@@ -0,0 +1,14 @@
+let formulario = document.querySelector(".fale_conosco")
+
+let mascara_form = document.querySelector(".mascara_form")
+
+function fale_conosco () {
+    formulario.style.left = "500px"
+    mascara_form.style.visibility = "visible"
+}
+
+function fechaForm () {
+    formulario.style.left = "-320"
+    console.log("foi")
+}
+
