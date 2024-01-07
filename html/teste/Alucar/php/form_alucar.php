ec70c65ad07b061dd070f4824596970ee894bcbe
Eliel S <hydrahtec@gmail.com>
2024-01-07 17:39:54 -0300

    estruta, css e recuperação de dados do formulario pagina alucar

---
+++ html/teste/Alucar/php/form_alucar.php
@@ -0,0 +1,28 @@
+<?php
+  //Variáveis
+  $nome = $_POST['nome'];
+  $data_nascimento = $_POST['data_nascimento'];
+  $telefone = $_POST['telefone'];
+  $numero_registro = $_POST['numero_registro'];
+  $validade = $_POST['validade'];
+  $carro = $_POST['carro'];
+  $data_envio = date('d/m/Y');
+  $hora_envio = date('H:i:s');
+
+  //Compo E-mail
+  $arquivo = "Nome: ".$nome."\n"."Data de nascimento: ".$data_nascimento."\n"."Telefone: ".$telefone."\n"."CNH: ".$numero_registro."\n"."Validade: ".$validade."\n"."Carro: ".$carro."\n"."Este pedido foi enviado em ".$data_envio." ás ".$hora_envio;
+  
+  //Emails para quem será enviado o formulário
+  $destino = "hydrahtec@gmail.com";
+  $assunto = "Alucar - aluguel de carro";
+
+  //Este sempre deverá existir para garantir a exibição correta dos caracteres
+  $headers  = "MIME-Version: 1.0\n";
+  $headers .= "Content-type: text/html; charset=UTF-8\n";
+  $headers .= "From: $nome";
+
+  //Enviar
+  mail($destino, $assunto, $arquivo, $headers);
+  
+  echo "<meta http-equiv='refresh' content='10;URL=./contato.html'>";
+?>
