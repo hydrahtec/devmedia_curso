93ee11ea75ed05901f32d93547a0f78ebf2dcac4
Eliel Silva <hydrahtec@gmail.com>
2024-04-15 15:58:59 -0300

    docs: react projeto troca de bg v1.0

--- html/react/troca_bg/src/pages/components/Troca_bg/index.jsx
+++ html/react/troca_bg/src/pages/components/Troca_bg/index.jsx
@@ -1,10 +1,20 @@
+import { useState } from 'react';
+import style from './Troca_bg.module.css';
+
 export const Troca_bg = () => {
+    let [nova_bg, setNova_bg] = useState(style.bg_vermelho);
 
-
-    
+    function trocaCor () {
+        let novaCor = nova_bg != style.bg_vermelho ? style.bg_vermelho : style.bg_azul;
+        setNova_bg(novaCor);
+    }
     return (
-        <main>
-
-        </main>
+        <section className={style.secao}>
+            <div className= {nova_bg}></div>
+            <div className={style.conteudo}>
+                <p>Clique no botão abaixo para mudar a o fundo do quadrado</p>
+                <button className={style.botao} onClick={trocaCor}>Trocar fundo</button>
+            </div>
+        </section>
     )
 };
