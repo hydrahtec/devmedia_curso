470b4a4697a49c374097fb11bf62bece50994a4f
Eliel Silva <hydrahtec@gmail.com>
2024-04-19 12:35:53 -0300

    docs: menu 06, part 2

---
+++ html/front-end/menu/menu_06/asset/js/script.js
@@ -0,0 +1,9 @@
+const menuItem = document.querySelectorAll('.item_menu');
+menuItem.forEach((item) => {
+    item.addEventListener('click', () => {
+       menuItem.forEach((item) => {
+        item.classList.remove('active');
+       })
+       item.classList.add('active');
+    });
+});
