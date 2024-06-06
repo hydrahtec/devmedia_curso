feec44970503d0ce55ee9ebcab8577ac63767e73
Eliel Silva <hydrahtec@gmail.com>
2024-06-06 16:25:30 -0300

    docs: to do list 01 variables

--- html/JavaScript/desafios/to do list/to do 01/js/index.js
+++ html/JavaScript/desafios/to do list/to do 01/js/index.js
@@ -1,1 +1,12 @@
-{}
+// Seleção de elementos
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelectot("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelEditBtn = document.querySelector("cancel-edit-btn");
+const searchInput = document.querySelector("search-input");
+const eraseBtn = document.querySelector("erase-button");
+const filterBtn = document.querySelector("#filter-select");
+
+let oldInputValue;
