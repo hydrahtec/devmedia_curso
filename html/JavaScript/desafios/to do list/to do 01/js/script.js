ac9b88e735de71bf241174a32fc56fb02bf5821c
Eliel Silva <hydrahtec@gmail.com>
2024-06-15 23:04:49 -0300

    docs: javascript to do 01 selecão de elementos

---
+++ html/JavaScript/desafios/to do list/to do 01/js/script.js
@@ -0,0 +1,28 @@
+// Select elements
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+
+
+const editForm = document.querySelector("#edit-form");
+
+
+const todoList = document.querySelector("#todo-List");
+
+
+const todoForm = document.querySelector("#todo-form");
+const todoForm = document.querySelector("#todo-form");
+const todoForm = document.querySelector("#todo-form");
+const todoForm = document.querySelector("#todo-form");
+
+
+
+
+
+
+
+
+// create functions
+
+//add events
+
+// localStorage
