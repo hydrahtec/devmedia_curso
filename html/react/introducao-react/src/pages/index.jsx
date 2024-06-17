7919e467622fc67e0a095cc7aba715dcc3116d89
Eliel Silva <hydrahtec@gmail.com>
2024-06-17 17:33:50 -0300

    docs: react introdução style home

--- html/react/introducao-react/src/pages/index.jsx
+++ html/react/introducao-react/src/pages/index.jsx
@@ -18,34 +18,34 @@ export default function Home() {
         </header>
 
         <main>
-          <section className={styles.title}>
+          <section className={styles.section_title}>
             <h1>React js</h1>
             <p>Biblioteca javascript para criar SPAs</p>
             <a href="#">Site oficial</a>
           </section>
 
-          <section className={styles.requisitos}>
+          <section className={styles.section_requisitos}>
             <h2>Requisitos</h2>
             <p>Veja abaixo os requisitos minimos para se aprender react JS</p>
-            <div>
+            <div className={styles.container_cards}>
               <div>
                 <h3>HTML</h3>
                 <p>Aprenda a estruturar os elementos da página com HTML</p>
               </div>
               <div>
-                <h3>css</h3>
+                <h3>CSS</h3>
                 <p>Aprenda a estilizar os elementos da página com CSS</p>
               </div>
               <div>
-                <h3>JavaScript</h3>
+                <h3>JAVASCRIPT</h3>
                 <p>Aprenda a linguagem utilizada pelo react, o JavaScript</p>
               </div>
             </div>
           </section>
         </main>
 
-        <footer>
-          <p>desenvolvido por Hydrah Tech</p>
+        <footer className={styles.footer}>
+          <p>Desenvolvido por Hydrah Tech</p>
         </footer>
       </div>
     </>
