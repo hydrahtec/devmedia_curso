a8849f5abe634c91bd2c518d81c31fde52c75ed2
Eliel Silva <hydrahtec@gmail.com>
2024-06-18 16:13:18 -0300

    docs: react exemplo pratico links css

--- html/react/exmplo_pratico_nav/src/components/Topo/index.jsx
+++ html/react/exmplo_pratico_nav/src/components/Topo/index.jsx
@@ -7,9 +7,9 @@ export default function Topo() {
         <header className={style.container_topo}>
             <h1>Navegação React</h1>
             <nav>
-                <Link href="/frontend">Front-end</Link>
-                <Link href="/backend">Back-end</Link>
-                <Link href="/mobile">Mobile</Link>
+                <Link className={style.link_frontend} href="/frontend">Front-end</Link>
+                <Link className={style.link_backend} href="/backend">Back-end</Link>
+                <Link className={style.link_mobile} href="/mobile">Mobile</Link>
             </nav>
         </header>
     );
