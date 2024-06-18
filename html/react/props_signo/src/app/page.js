72dc32828716be2b9863bd8f0954b1fe257d5865
Eliel Silva <hydrahtec@gmail.com>
2024-06-18 18:29:31 -0300

    docs: react props signo estrutura do projeto

---
+++ html/react/props_signo/src/app/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      
+    </main>
+  );
+}
