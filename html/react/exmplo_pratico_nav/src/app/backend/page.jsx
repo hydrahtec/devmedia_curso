72fad489a8e8adcaac4be0ad526686af24097285
Eliel Silva <hydrahtec@gmail.com>
2024-06-18 15:48:14 -0300

    docs: react exemplo pratico page front end

---
+++ html/react/exmplo_pratico_nav/src/app/backend/page.jsx
@@ -0,0 +1,9 @@
+import style from './Backend.module.css';
+import Link from 'next/link';
+
+export default function Backend() {
+
+    return (
+        
+    );
+};
--- html/react/exmplo_pratico_nav/src/app/frontend/page.jsx
+++ html/react/exmplo_pratico_nav/src/app/frontend/page.jsx
@@ -1,10 +1,24 @@
+import Link from 'next/link';
 import style from './Page.module.css';
 
-export default function frontend () {
+export default function Frontend() {
 
     return (
-        <main>
-            
-        </main>
+        <div className={style.container_frontend}>
+            <h2>React.JS</h2>
+            <p>
+                O React.js é uma biblioteca JavaScript utilizada para construir
+                interfaces de usuário interativas e eficientes. Ele é amplamente adotado
+                devido à sua abordagem baseada em componentes e à facilidade de criar
+                aplicativos escaláveis. O React permite o desenvolvimento de Single Page
+                Applications (SPAs) de forma eficiente e flexível.
+            </p>
+            <Link
+                href="https://www.devmedia.com.br/carreira-programador/?slug=front-end"
+                target="_blank"
+            >
+                Guia de Front-end
+            </Link>
+        </div>
     );
 };
