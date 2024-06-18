acfc836a8be915b70a422ea08174737d8ba96b89
Eliel Silva <hydrahtec@gmail.com>
2024-06-18 15:57:34 -0300

    docs: react exemplo pratico page mobile

---
+++ html/react/exmplo_pratico_nav/src/app/mobile/page.jsx
@@ -0,0 +1,26 @@
+import style from './Mobile.module.css';
+import Link from 'next/link';
+
+export default function Mobile() {
+
+    return (
+        <div className={style.container_mobile}>
+            <h2>React Native</h2>
+            <p>
+            React Native é um framework de desenvolvimento de aplicativos móveis que
+            permite a criação de aplicativos nativos usando JavaScript e React. Ele
+            permite o desenvolvimento de aplicativos para iOS e Android usando uma
+            base de código compartilhada, proporcionando uma experiência de
+            desenvolvimento eficiente e rápida. Com React Native, os desenvolvedores
+            podem criar aplicativos móveis robustos e de alto desempenho de forma
+            simplificada.
+            </p>
+            <Link
+            href="https://www.devmedia.com.br/carreira-programador/?slug=mobile"
+            target="_blank"
+            >
+            Guia de Mobile
+            </Link>
+        </div>
+    );
+};
