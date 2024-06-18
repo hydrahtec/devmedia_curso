c3ada373ab0073ee917f3b7e42476679f06963fa
Eliel Silva <hydrahtec@gmail.com>
2024-06-18 19:43:20 -0300

    docs: react footer component

--- html/react/props_signo/src/app/layout.js
+++ html/react/props_signo/src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
+import Footer from "@/components/footer";
 import Topo from "@/components/topo";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,6 +20,7 @@ export default function RootLayout({ children }) {
         
         {children}
         
+        <Footer />
       </body>
     </html>
   );
