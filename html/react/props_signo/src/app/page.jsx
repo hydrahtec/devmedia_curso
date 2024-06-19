e3f7ad692939c2f8e25d470b01b972fe80d4a4cc
Eliel Silva <hydrahtec@gmail.com>
2024-06-19 14:26:33 -0300

    docs: react props exemplo pratico component card

--- html/react/props_signo/src/app/page.jsx
+++ html/react/props_signo/src/app/page.jsx
@@ -1,15 +1,10 @@
-import Link from "next/link";
 import styles from "./page.module.css";
-import Image from "next/image";
+import CardSigno from "@/components/cardSigno";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div>
-        <h3>Titulo</h3>
-        <img src="../aquario.jpg" alt="teste" />
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-      </div>
+      <CardSigno />
     </main>
   );
 }
