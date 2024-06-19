51ebd6db274386bbb263449aab3b89708f84845a
Eliel Silva <hydrahtec@gmail.com>
2024-06-19 15:45:43 -0300

    docs: react props exemplo pratico paginas de signos

---
+++ html/react/props_signo/src/app/touro/page.jsx
@@ -0,0 +1,18 @@
+import style from './Aquarios.module.css';
+
+import Image from 'next/image';;
+import Link from 'next/link';;
+
+export default function Touro() {
+    return (
+        <main className={style.main}>
+            <div className={style.page}>
+                <h1>Touro</h1>
+                <Image src="/touro.jpg" width='300' height = '300' alt=''/>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, obcaecati excepturi. Aspernatur dolores ea vel temporibus, ipsum distinctio modi. Quae quibusdam nobis architecto quo aut corrupti sint aliquam nostrum neque!</p>
+                <Link className='link_home' href='/'>Voltar</Link>
+            </div>
+        </main>
+    );
+
+}
