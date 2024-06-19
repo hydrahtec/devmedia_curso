62e170073ff6a3110dd4a98ea941d82237053d1d
Eliel Silva <hydrahtec@gmail.com>
2024-06-19 12:37:50 -0300

    docs: javascript to do 01 v5 function saveTodo

--- html/JavaScript/desafios/to do list/to do 01/js/script.js
+++ html/JavaScript/desafios/to do list/to do 01/js/script.js
@@ -56,7 +56,42 @@ const updateTodoLocalStorage = (oldInputValue, newInputValue) => {
     localStorage.setItem('todos', JSON.stringify(todos));
 };
 // funções
-const saveTodo = (text, done = 0, save = 1) => {};
+const saveTodo = (text, done = 0, save = 1) => {
+    const todo = document.createElement('div');
+    todo.classList.add('todo');
+
+    const titletodo = document.createElement('h3');
+    titletodo.textContent = text;
+    todo.appendChild(titletodo);
+
+    const doneBtn = document.createElement('button');
+    doneBtn.classList.add('finish-todo');
+    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
+    todo.appendChild(doneBtn);
+
+    const editBtn = document.createElement('button');
+    editBtn.classList.add('edit-todo');
+    editBtn.innerHTML = '<i class="fa-solid fa-pen">';
+    todo.appendChild(editBtn);
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('remove-todo');
+    deleteBtn.innerHTML = '<i class="fa-solid fa-xmark">';
+    todo.appendChild(deleteBtn);
+
+    if (done) {
+        todo.classList.add('done')
+    }
+    if (save) {
+        saveTodoLocalStorage({text, done: 0});
+    }
+
+    todoList.appendChild(todo);
+
+    todoInput.value = "";
+
+    todoInput.focus();
+};
 const toggleForms = () => {
     todoForm.classList.toggle('hide');
     todoList.classList.toggle('hide');
