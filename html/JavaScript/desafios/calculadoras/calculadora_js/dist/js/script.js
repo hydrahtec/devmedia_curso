d619b24bce9b65176274c176fadd55c6f99981aa
Eliel Silva <hydrahtec@gmail.com>
2024-06-24 14:19:11 -0300

    docs: js calculadora 01 estrutura class calculator

--- html/JavaScript/desafios/calculadoras/calculadora_js/dist/js/script.js
+++ html/JavaScript/desafios/calculadoras/calculadora_js/dist/js/script.js
@@ -2,5 +2,15 @@
 const previousOperationText = document.querySelector('#previous_operation');
 const currentOperationText = document.querySelector('#current_operation');
 const buttons = document.querySelectorAll('#buttons_container button');
+class Calculator {
+    constructor(previousOperationText, currentOperationText) {
+        this.previousOperationText = previousOperationText;
+        this.currentOperationText = currentOperationText;
+        this.currentOperation = "";
+    }
+    ;
+}
+;
+const calc = new Calculator(previousOperationText, currentOperationText);
 buttons.forEach(() => {
 });
--- html/JavaScript/desafios/calculadoras/calculadora_js/src/ts/script.ts
+++ html/JavaScript/desafios/calculadoras/calculadora_js/src/ts/script.ts
@@ -3,6 +3,32 @@ const previousOperationText: Element | null = document.querySelector('#previous_
 const currentOperationText: Element | null = document.querySelector('#current_operation');
 const buttons: NodeListOf<Element> = document.querySelectorAll('#buttons_container button');
 
+class Calculator {
+    constructor (previousOperationText: Element | null, currentOperationText: Element | null) {
+        this.previousOperationText = previousOperationText;
+        this.currentOperationText = currentOperationText;
+        this.currentOperation = "";
+    };
+
+    //add digit to calculator screen
+
+    //processe all calculator operation
+
+    //change values of calculator screen
+
+    //change math operation
+
+    //DEL a digit
+
+    //clear current operation
+
+    //clear all operation
+
+    //processe an operation
+};
+
+const calc = new Calculator(previousOperationText, currentOperationText);
+
 buttons.forEach(() => {
-    
+
 });
