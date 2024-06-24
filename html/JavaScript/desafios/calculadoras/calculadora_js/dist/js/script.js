945ed0a86799f3bdff4c629672c6a1dcc921562f
Eliel Silva <hydrahtec@gmail.com>
2024-06-24 14:04:47 -0300

    docs: js calculadora 01 selecao de elementos

--- html/JavaScript/desafios/calculadoras/calculadora_js/dist/js/script.js
+++ html/JavaScript/desafios/calculadoras/calculadora_js/dist/js/script.js
@@ -1,1 +1,6 @@
 "use strict";
+const previousOperationText = document.querySelector('#previous_operation');
+const currentOperationText = document.querySelector('#current_operation');
+const buttons = document.querySelectorAll('#buttons_container button');
+buttons.forEach(() => {
+});
---
+++ html/JavaScript/desafios/calculadoras/calculadora_js/src/ts/script.ts
@@ -0,0 +1,8 @@
+//selecção de elementos
+const previousOperationText: Element | null = document.querySelector('#previous_operation');
+const currentOperationText: Element | null = document.querySelector('#current_operation');
+const buttons: NodeListOf<Element> = document.querySelectorAll('#buttons_container button');
+
+buttons.forEach(() => {
+    
+});
